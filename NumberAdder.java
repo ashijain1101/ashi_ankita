6d9d82057b2259a1fad498ccfedc85957b117778
unknown <Administrator@dac20.students.acts.in>
2023-01-25 15:40:46 +0530

    comment

---
+++ NumberAdder.java
@@ -0,0 +1,11 @@
+public class NumberAdder {
+			public static void main (String[] pratik){
+			
+			int a= Integer.parseInt(pratik[0]);
+			int b= Integer.parseInt(pratik[1]);
+			//int b= Integer.parseInt(pratik[1]);
+			int sum= a+b;
+			System.out.println("sum="+sum);
+			
+			}
+			}
---
+++ addition.java
@@ -0,0 +1,8 @@
+public class addition{
+public static void main(String[] args){
+float a=10.50f;
+int b=20;
+float c=a+b;
+System.out.println(c);
+}
+}
